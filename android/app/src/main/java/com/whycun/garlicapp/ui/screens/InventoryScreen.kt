0a24c0a5623593b2c40d19b3317d9f38d7b8e758
whycun <316860426@qq.com>
2026-06-14 19:18:06 +0800

    fix: add missing clickable import

--- android/app/src/main/java/com/whycun/garlicapp/ui/screens/InventoryScreen.kt
+++ android/app/src/main/java/com/whycun/garlicapp/ui/screens/InventoryScreen.kt
@@ -1,6 +1,7 @@
 package com.whycun.garlicapp.ui.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
